b11680a9f9a1d0e0b0fc67757604798df655e914
wendyyanto <yanto.wendy98@gmail.com>
2020-05-19 00:34:30 +0700

    Refactor Main Class

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,11 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Container;
-import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
-import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -21,141 +14,167 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 public class Main extends JFrame {
-	
-	final int width = 800;
-	final int height = 500;
+
+	private static final int GAME_WIDTH = 800;
+	private static final int GAME_HEIGHT = 500;
+
+	private JPanel infoPanel = new JPanel();
+	private JPanel titlePanel = new JPanel();
+	private JPanel timeLeftPanel = new JPanel();
+	private JPanel totalLifePanel = new JPanel();
+	private JPanel currentLevelPanel = new JPanel();
+	private JPanel blueBoxPanel = new JPanel();
+	private JPanel greenBoxPanel = new JPanel();
+	private JPanel yellowBoxPanel = new JPanel();
+	private JPanel redBoxPanel = new JPanel();
+	private JPanel pauseInfoPanel = new JPanel();
+	private JPanel exitPanel = new JPanel();
+
 	static JLabel timeLeft;
 	static JLabel totalLife;
 	static JLabel currLevel;
-	
-	public Main() {
-		setLayout(new BorderLayout());
-		
-		GamePanel gamePanel = new GamePanel();
-		add(gamePanel,BorderLayout.CENTER);
-		
-		JPanel infoPanel = new JPanel();
+
+	private void setupInfoPanel() {
 		infoPanel.setSize(400,400);
 		GridLayout Legends = new GridLayout(12,1);
 		infoPanel.setLayout(Legends);
-		
-		JPanel titlePanel = new JPanel();
+	}
+
+	private void setupTitlePanel() {
 		JLabel title = new JLabel("Find the X-it");
 		title.setFont(new Font("Serif", Font.PLAIN, 30));
 		title.setHorizontalAlignment(SwingConstants.LEFT);
-
 		titlePanel.add(title);
-		infoPanel.add(titlePanel);
-		
-		JPanel timeLeftPanel = new JPanel();
+	}
+
+	private void setupTimeLeftPanel() {
 		timeLeft = new JLabel("Time Left : 25");
 		timeLeft.setFont(new Font("Serif", Font.PLAIN, 24));
 		timeLeft.setVerticalAlignment(SwingConstants.CENTER);
-		
 		timeLeftPanel.add(timeLeft);
-		infoPanel.add(timeLeftPanel);
-		
-		JPanel totalLifePanel = new JPanel();
+	}
+
+	private void setupTotalLifePanel() {
 		totalLife = new JLabel("Life : 3");
 		totalLife.setFont(new Font("Serif", Font.PLAIN, 24));
-		
 		totalLifePanel.add(totalLife);
-		infoPanel.add(totalLifePanel);	
-		
-		JPanel currLevelPanel = new JPanel();
+	}
+
+	private void setupCurrentLevelPanel() {
 		currLevel = new JLabel("Level : 1");
 		currLevel.setFont(new Font("Serif", Font.PLAIN, 24));
-		currLevelPanel.add(currLevel);
-		infoPanel.add(currLevelPanel);	
-		
-		JPanel blueBoxPanel = new JPanel();
-		ImageIcon blueBoxImage = new ImageIcon("src/BlueBox.png");
+		currentLevelPanel.add(currLevel);
+	}
+
+	private void setupBlueBoxPanel() {
+		ImageIcon blueBoxImage = new ImageIcon("src/assets/BlueBox.png");
 		JLabel blueBoxLabel = new JLabel("Your Goal");
 		blueBoxLabel.setIcon(blueBoxImage);
 		blueBoxPanel.add(blueBoxLabel);
-		infoPanel.add(blueBoxPanel);
-		
-		JPanel greenBoxPanel = new JPanel();
-		ImageIcon greenBoxImage = new ImageIcon("src/GreenBox.png");
+	}
+
+	private void setupGreenBoxPanel() {
+		ImageIcon greenBoxImage = new ImageIcon("src/assets/GreenBox.png");
 		JLabel greenBoxLabel = new JLabel("Player");
 		greenBoxLabel.setIcon(greenBoxImage);
 		greenBoxPanel.add(greenBoxLabel);
-		infoPanel.add(greenBoxPanel);
-		
-		JPanel yellowBoxPanel = new JPanel();
-		ImageIcon yellowBoxImage = new ImageIcon("src/YellowBox.png");
+	}
+
+	private void setupYellowBoxPanel() {
+		ImageIcon yellowBoxImage = new ImageIcon("src/assets/YellowBox.png");
 		JLabel yellowBoxLabel = new JLabel("Coin (Extra Time)");
 		yellowBoxLabel.setIcon(yellowBoxImage);
 		yellowBoxPanel.add(yellowBoxLabel);
-		infoPanel.add(yellowBoxPanel);		
-		
-		JPanel redBoxPanel = new JPanel();
-		ImageIcon redBoxImage = new ImageIcon("src/RedBox.png");
+	}
+
+	private void setupRedBoxPanel() {
+		ImageIcon redBoxImage = new ImageIcon("src/assets/RedBox.png");
 		JLabel redBoxLabel = new JLabel("Trap");
 		redBoxLabel.setIcon(redBoxImage);
 		redBoxPanel.add(redBoxLabel);
-		infoPanel.add(redBoxPanel);
-		
-		JPanel spacePressPanel = new JPanel();
+	}
+
+	private void setupPauseInfoPanel() {
 		JLabel spaceLabel = new JLabel("Press 'Space' to Pause the game");
 		spaceLabel.setFont(new Font("Serif", Font.PLAIN, 20));
 		spaceLabel.setForeground(Color.decode("#401ef9"));
-		spacePressPanel.add(spaceLabel);
-		infoPanel.add(spacePressPanel);
-		
-		JPanel ClosePanel = new JPanel();
+		pauseInfoPanel.add(spaceLabel);
+	}
+
+	private void setupExitPanel() {
 		JLabel CloseHoverActionLabel = new JLabel("Hover this to show exit button");
 		JButton CloseButton = new JButton("Close");
 		CloseButton.setRolloverEnabled(false);
 		CloseButton.setVisible(false);
-		
-		CloseButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
-		});
-		
+
+		CloseButton.addActionListener(e -> System.exit(0));
+
 		CloseButton.addMouseListener(new MouseAdapter() {
-			
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				CloseButton.setVisible(true);
 			}
-			
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 				CloseButton.setVisible(false);
 			}
 		});
-		
-		ClosePanel.addMouseListener(new MouseAdapter() {
-			
+
+		exitPanel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				CloseButton.setVisible(true);
 			}
-			
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 				CloseButton.setVisible(false);
 			}
 		});
-		
-		ClosePanel.add(CloseHoverActionLabel);
-		ClosePanel.add(CloseButton);
-		infoPanel.add(ClosePanel);
-		
-		add(infoPanel,BorderLayout.EAST);
-		setSize(width,height);
+
+		exitPanel.add(CloseHoverActionLabel);
+		exitPanel.add(CloseButton);
+	}
+
+	private Main() {
+		setLayout(new BorderLayout());
+
+		GamePanel gamePanel = new GamePanel();
+		add(gamePanel,BorderLayout.CENTER);
+
+		setupInfoPanel();
+		setupTitlePanel();
+		setupTimeLeftPanel();
+		setupTotalLifePanel();
+		setupCurrentLevelPanel();
+		setupBlueBoxPanel();
+		setupGreenBoxPanel();
+		setupYellowBoxPanel();
+		setupRedBoxPanel();
+		setupPauseInfoPanel();
+		setupExitPanel();
+
+		infoPanel.add(titlePanel);
+		infoPanel.add(timeLeftPanel);
+		infoPanel.add(totalLifePanel);
+		infoPanel.add(currentLevelPanel);
+		infoPanel.add(blueBoxPanel);
+		infoPanel.add(greenBoxPanel);
+		infoPanel.add(yellowBoxPanel);
+		infoPanel.add(redBoxPanel);
+		infoPanel.add(pauseInfoPanel);
+		infoPanel.add(exitPanel);
+
+		add(infoPanel, BorderLayout.EAST);
+		setSize(GAME_WIDTH, GAME_HEIGHT);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setVisible(true);	
+		setVisible(true);
 	}
 
 	public static void main(String[] args) {
 		new Main();
 	}
-
 
 }
